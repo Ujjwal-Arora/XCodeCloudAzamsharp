f2c9eace9ee6de027ca15898175c44fb24cca8f7
Ujjwal Arora <ukarora9@gmail.com>
2025-03-05 01:16:50 +0530

    Initial Commit

---
+++ XCodeCloudAzamsharp/XCodeCloudAzamsharpApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeCloudAzamsharpApp.swift
+//  XCodeCloudAzamsharp
+//
+//  Created by Ujjwal Arora on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct XCodeCloudAzamsharpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
